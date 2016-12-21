e12abe8f36b871ea6eebcaab994742b468253f5f
qianlidong <qianlidongjava@163.com>
2016-12-21 09:55:56 +0800

    121212

--- app-root/app-core/src/main/java/com/lxy/app/core/service/BaseService.java
+++ app-root/app-core/src/main/java/com/lxy/app/core/service/BaseService.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface BaseService {
 	void save(Object entity);
 
-	//我来修改下 qld 20161221
+	//我来修改下 qld 20161221 121212
 	<T> T getById(Class<T> clazz, Object id);
 
 }
