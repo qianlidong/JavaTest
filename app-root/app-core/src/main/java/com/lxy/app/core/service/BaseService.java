19f84bdc11ffd57b89f028b71120aa23779cb82b
sks <sks@lxy-PC>
2016-12-21 10:30:46 +0800

    1111

--- app-root/app-core/src/main/java/com/lxy/app/core/service/BaseService.java
+++ app-root/app-core/src/main/java/com/lxy/app/core/service/BaseService.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface BaseService {
 	void save(Object entity);
 
-	//我来修改下 qld 20161221 121212
+	//我来修改下 qld 20161221 121212 QQQQ
 	<T> T getById(Class<T> clazz, Object id);
 	
 	
